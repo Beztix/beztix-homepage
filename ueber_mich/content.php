193e23de9378127cf4ce9a3f80437936715ac515
Sebastian Luetzow <sebastian.luetzow@gmail.com>
2016-08-24 19:27:17 +0200

    added data about me and skills

--- ueber_mich/content.php
+++ ueber_mich/content.php
@@ -10,7 +10,162 @@ if(!defined('AccessConstant')) {die('Direct access not permitted');}
 
 				<h1>Über mich</h1>
 			
+				<h3>Daten</h3>
 				
+				<table class="personal_data">
+					<tr>
+						<td class="left">Name:</td>
+						<td class="right">Sebastian Lützow</td>
+					</tr>
+					<tr>
+						<td class="left">Spitzname:</td>
+						<td class="right">Basti</td>
+					</tr>
+					<tr>
+						<td class="left">Nickname:</td>
+						<td class="right">Beztix</td>
+					</tr>
+					<tr>
+						<td class="left">Geburtstag:</td>
+						<td class="right">25.06.1991</td>
+					</tr>
+					<tr>
+						<td class="left">Studium:</td>
+						<td class="right">Informatik B.Sc.</td>
+					</tr>
+					<tr>
+						<td class="left">Wohnort</td>
+						<td class="right">Bayreuth</td>
+					</tr>
+				</table>
+				
+				
+				
+				
+				<h3>Skills</h3>
+				
+				<table class="personal_skills">
+					<tr>
+						<td class="left1">
+							HTML/CSS
+							<img class="skills_icon" src="../_images_content/html.png" alt="html icon">
+						</td>
+						<td class="right1">
+							&#9724;&#9724;&#9724;&#9724;&#9723;
+						</td>
+						
+						<td class="left2">
+							PHP
+							<img class="skills_icon" src="../_images_content/php.png" alt="php icon">
+						</td>
+						<td class="right2">
+							&#9724;&#9724;&#9724;&#9724;&#9723;
+						</td>
+					</tr>
+					<tr>
+						<td class="left1">
+							JavaScript
+							<img class="skills_icon" src="../_images_content/javascript.png" alt="javascript icon">
+						</td>
+						<td class="right1">
+							&#9724;&#9723;&#9723;&#9723;&#9723;
+						</td>
+						
+						<td class="left2">
+							(Linux-)Server
+							<img class="skills_icon" src="../_images_content/debian.png" alt="debian icon">
+						</td>
+						<td class="right2">
+							&#9724;&#9724;&#9724;&#9723;&#9723;
+						</td>
+					</tr>
+					<tr>
+						<td class="left1">
+							C
+							<img class="skills_icon" src="../_images_content/c.png" alt="c icon">
+						</td>
+						<td class="right1">
+							&#9724;&#9724;&#9723;&#9723;&#9723;
+						</td>
+						
+						<td class="left2">
+							C++
+							<img class="skills_icon" src="../_images_content/cplusplus.png" alt="cplusplus icon">
+						</td>
+						<td class="right2">
+							&#9724;&#9724;&#9724;&#9723;&#9723;
+						</td>
+					</tr>
+					<tr>
+						<td class="left1">
+							Java
+							<img class="skills_icon" src="../_images_content/java.png" alt="java icon">
+						</td>
+						<td class="right1">
+							&#9724;&#9724;&#9724;&#9724;&#9723;
+						</td>
+						
+						<td class="left2">
+							Latex
+							<img class="skills_icon" src="../_images_content/latex.png" alt="latex icon">
+						</td>
+						<td class="right2">
+							&#9724;&#9724;&#9724;&#9724;&#9723;
+						</td>
+					</tr>
+					<tr>
+						<td class="left1">
+							Git
+							<img class="skills_icon" src="../_images_content/git.png" alt="git icon">
+						</td>
+						<td class="right1">
+							&#9724;&#9724;&#9724;&#9723;&#9723;
+						</td>
+						
+						<td class="left2">
+							?
+						</td>
+						<td class="right2">
+							&#9723;&#9723;&#9723;&#9723;&#9723;
+						</td>
+					</tr>
+					<tr>
+						<td class="left1">
+							Adobe Photoshop
+							<img class="skills_icon" src="../_images_content/photoshop.png" alt="photoshop icon">
+						</td>
+						<td class="right1">
+							&#9724;&#9724;&#9724;&#9724;&#9723;
+						</td>
+						
+						<td class="left2">
+							Adobe Illustrator
+							<img class="skills_icon" src="../_images_content/illustrator.png" alt="illustrator icon">
+						</td>
+						<td class="right2">
+							&#9724;&#9724;&#9724;&#9723;&#9723;
+						</td>
+					</tr>
+					<tr>
+						<td class="left1">
+							Adobe Premiere Pro
+							<img class="skills_icon" src="../_images_content/premiere.png" alt="premiere icon">
+						</td>
+						<td class="right1">
+							&#9724;&#9724;&#9723;&#9723;&#9723;
+						</td>
+						
+						<td class="left2">
+							Office-Software
+							<img class="skills_icon" src="../_images_content/office.png" alt="office icon">
+						</td>
+						<td class="right2">
+							&#9724;&#9724;&#9724;&#9723;&#9723;
+						</td>
+					</tr>
+					
+
+				</table>
 
 			</section>
 			
